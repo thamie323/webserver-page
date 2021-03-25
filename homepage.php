ae1d2e8bf6c1065aaa99b3fb3f92b49959ffc4c6
thamie323 <62136298+thamie323@users.noreply.github.com>
2021-03-25 13:49:51 -0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,17 @@
+
+<body>
+<div>
+	<div>
+	<form enctype="multipart/form-data" action="phpfile.php" method="POST">
+  <input type="file" name="file" id="file">
+  <input type="submit" id="btn_uploadfile" value="upload">
+	 </form>
+	 </div>
+	 
+	 
+</div>
+<script>
+// Upload file
+
+</script>
+</body>
